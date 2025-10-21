6141ae9249f985f3c4b6751a858698f56744d006
Chrstxxan <chris.delucca.2006@gmail.com>
2025-10-21 13:25:53 -0300

    fix: cors trailing slash

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ const PORT = process.env.PORT || 5000;
 // ================================
 // Configuração de Middlewares
 // ================================
+const frontOrigin = (process.env.FRONT_ORIGIN || 'http://127.0.0.1:5500').replace(/\/$/, '');
+
 app.use(
   cors({
-    origin: process.env.FRONT_ORIGIN || 'http://127.0.0.1:5500',
+    origin: frontOrigin,
     credentials: true,
   })
 );
